f62541f4309cc5b68799db516671e01e8d3d04c8
안유나 <sgvina@naver.com>
2023-11-16 17:49:23 +0900

    1116

--- final-project-front/src/router/index.js
+++ final-project-front/src/router/index.js
@@ -1,23 +1,21 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import MainPageView from "@/views/MainPageView.vue";
+import ExchangeView from "@/views/ExchangeView.vue";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
-      path: '/',
-      name: 'home',
-      component: HomeView
+      path: "/",
+      name: "home",
+      component: MainPageView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+      path: "/exchange",
+      name: "exchange",
+      component: ExchangeView,
+    },
+  ],
+});
 
-export default router
+export default router;
---
+++ final-project-front/src/views/ExchangeView.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+    <h1>환율 계산기</h1>
+  </div>
+</template>
+
+<script setup>
+import { ref, computed } from "vue";
+import axios from "axios";
+
+// 환율 정보 받을 배열 생성
+const exchangeInfo = ref([]);
+
+const today = new Date();
+const year = today.getFullYear();
+const month = today.getMonth();
+const day = today.getDay();
+
+console.log(`${year}${month}${day}`);
+
+const API_URL =
+  "https://www.koreaexim.go.kr/site/program/financial/exchangeJSON";
+const api_key = "JCRjUvmotEolcA327lzEmt4EWJN97g16";
+
+const serarchExchage = () => {
+  axios.get(API_URL, {
+    params: {
+      key: api_key,
+      searchdate: 20231116,
+      data: "AP010",
+    },
+  });
+};
+</script>
+
+<style scoped></style>
