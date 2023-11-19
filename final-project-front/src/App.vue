0c9d002fec188e098ad3b770fe6add0a4954198b
박정환 <jeonghw4n@naver.com>
2023-11-19 17:17:51 +0900

    Update final-project-front/src/App.vue

--- final-project-front/src/App.vue
+++ final-project-front/src/App.vue
@@ -17,6 +17,11 @@
             >Product</RouterLink
           >
           <span> | </span>
+
+          <RouterLink :to="{ name: 'article' }">Article</RouterLink>
+
+          <span> | </span>
+
           <RouterLink :to="{ name: 'signup' }" class="nav-link"
             >Signup</RouterLink
           >
@@ -37,18 +42,6 @@
 import { RouterLink, RouterView } from "vue-router";
 </script>
 
-<template>
-  <header>
-    <div>
-      <nav>
-        <RouterLink :to="{ name: 'home' }">Home</RouterLink> |
-        <RouterLink :to="{ name: 'exchange' }">Exchange</RouterLink> |
-        <RouterLink :to="{ name: 'article' }">Article</RouterLink>
-      </nav>
-    </div>
-  </header>
-  <RouterView />
-</template>
 
 <style scoped>
 .container-fluid {
