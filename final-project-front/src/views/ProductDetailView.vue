5bff3342062641551e547c6ca4bdf65e842b8f9a
박정환 <jeonghw4n@naver.com>
2023-11-23 13:24:12 +0900

    Update final-project-front/src/views/ProductDetailView.vue

--- final-project-front/src/views/ProductDetailView.vue
+++ final-project-front/src/views/ProductDetailView.vue
@@ -31,7 +31,7 @@
     <p></p>
     <p></p>
     <p></p>
-    <div @click="join" class="btn">가입하기</div>
+    <div @click="join(product)" class="btn">가입하기</div>
   </div>
 
   <!-- 
@@ -62,20 +62,11 @@ const product = computed(() => {
   )[0];
 });
 
-const join = () => {
-  axios({
-    method: "post",
-    url: `http://127.0.0.1:8000/products/${productCd}/${signStore.username}/`,
-  })
-    .then((response) => {
-      console.log(response.data);
-    })
-    .catch((error) => {
-      console.log(error);
-    });
-};
+const user = computed(() => {
+  return signStore.user;
+});
 
-// const user = ref();
+console.log(typeof user.value.financial_products);
 
 // const username = ref(null);
 // const email = ref(null);
@@ -84,69 +75,75 @@ const join = () => {
 // const age = ref(null);
 // const money = ref(null);
 // const salary = ref(null);
-// const financial_products = [];
-// const join = () => {
-//   console.log(username.value);
 
-//   const data = {
-//     username: username.value,
-//     // financial_products: [productCd],
-//   };
-//   if (email.value !== null && email.value !== "") {
-//     data.email = email.value;
-//   }
-//   if (age.value !== null && age.value !== "") {
-//     data.age = age.value;
-//   }
-//   if (nickname.value !== null && nickname.value !== "") {
-//     data.nickname = nickname.value;
-//   }
-//   if (money.value !== null && money.value !== "") {
-//     data.money = money.value;
-//   }
-//   if (salary.value !== null && salary.value !== "") {
-//     data.salary = salary.value;
-//   }
-//   if (financial_products.length > 0) {
-//     // if (financial_products.value.includes(productCd)) {
-//     //   alert("이미 가입한 상품입니다.");
-//     //   return;
-//     // } else {
-//     console.log(financial_products);
-//     // if (financial_products.value.length >= 1) {
-//     financial_products.push(productCd);
-//     console.log(financial_products);
-//     // financial_products.value = financial_products.value.join(",");
-//     // } else {
-//     // financial_products.value.push(productCd);
-//     // console.log(financial_products.value);
-//     // }
-//     data.financial_products = financial_products;
-//     // }
-//   } else {
-//     console.log(financial_products);
-//     financial_products.push(productCd);
-//     data.financial_products = financial_products;
-//     // console.log(flattenArray(data.financial_products));
-//   }
-//   axios({
-//     method: "put",
-//     url: "http://127.0.0.1:8000/accounts/update/",
-//     data: data,
-//     headers: {
-//       Authorization: `Token ${signStore.token}`,
-//     },
-//   })
-//     .then((response) => {
-//       alert("가입 완료 되었습니다.");
-//     })
-//     .catch((error) => {
-//       console.log(data);
-//       console.log(signStore.token);
-//       console.log(error);
-//       alert("이미 가입한 상품입니다.");
-//     });
-// };
+// const financial_products = ref([]);
+
+// const financial_products = computed(() => {
+//   return userInfo;
+// });
+// console.log(financial_products);
+const join = () => {
+  const data = {
+    // username: user.value.username,
+    email: user.value?.email,
+    financial_products: user.value?.financial_products,
+    age: user.value?.age,
+    money: user.value?.money,
+    salary: user.value?.salary,
+    nickname: user.value?.nickname,
+  };
+  if (
+    user.value.financial_products !== null &&
+    user.value.financial_products.length > 0
+  ) {
+    data.financial_products =
+      user.value.financial_products +
+      store.deposit_products?.filter(
+        (product) => product.fin_prdt_cd === productCd
+      )[0].fin_prdt_nm +
+      ", ";
+  } else {
+    data.financial_products =
+      store.deposit_products?.filter(
+        (product) => product.fin_prdt_cd === productCd
+      )[0].fin_prdt_nm + ", ";
+  }
+
+  if (user.value.email !== null && user.value.email !== "") {
+    data.email = user.value.email;
+  }
+  if (user.value.age !== null && user.value.age !== "") {
+    data.age = user.value.age;
+  }
+  if (user.value.nickname !== null && user.value.nickname !== "") {
+    data.nickname = user.value.nickname;
+  }
+  if (user.value.money !== null && user.value.money !== "") {
+    data.money = user.value.money;
+  }
+  if (user.value.salary !== null && user.value.salary !== "") {
+    data.salary = user.value.salary;
+  }
+
+  axios({
+    method: "put",
+    url: "http://127.0.0.1:8000/accounts/update/",
+    data: data,
+    headers: {
+      Authorization: `Token ${signStore.token}`,
+    },
+  })
+    .then((response) => {
+      console.log(data);
+      alert("가입 완료 되었습니다.");
+    })
+    .catch((error) => {
+      console.log(data);
+      console.log(signStore.token);
+      console.log(error);
+      alert("이미 가입한 상품입니다.");
+    });
+};
 
 // onMounted(() => {
 //   axios({
@@ -166,24 +163,20 @@ const join = () => {
 //       money.value = user.value.money;
 //       salary.value = user.value.salary;
 //       console.log(financial_products.value);
-//       if (user.value.financial_products !== null) {
+//       if (
+//         financial_products.value !== null &&
+//         financial_products.value !== `${[]}` &&
+//         user.value.financial_products !== null
+//       ) {
 //         console.log(user.value.financial_products);
-//         financial_products.push(user.value.financial_products);
+//         financial_products.value = user.value.financial_products.split(",");
 //       }
-//       // if (
-//       //   financial_products.value !== null &&
-//       //   financial_products.value !== `${[]}` &&
-//       //   user.value.financial_products !== null
-//       // ) {
-//       //   console.log(user.value.financial_products);
-//       //   financial_products.value = user.value.financial_products.split(",");
-//       // }
 
 //       // financial_products.append(financial_product);
 //       // if (financial_products.value.length >= 1) {
 //       //   financial_products.value = financial_products.value.join(",");
 //       // }
-//       console.log(financial_products);
+//       console.log(financial_products.value);
 //     })
 //     .catch((err) => {
 //       console.log(err);
